739aa09698a3d3817a37addf38e0eea2634ce8e4
maxnyquist <max.nyquist@mass.gov>
2022-01-12 17:00:50 -0500

    Option to set opening Day year instead of using Sys.Date

--- CreelSurveyStatistics.R
+++ CreelSurveyStatistics.R
@@ -70,12 +70,12 @@ config <- as.character(R_Config$CONFIG_VALUE)
   ### See the tables
   tables <- dbListTables(con, schema_name = schema) %>% print()
 ### Creel Tables   
-  Creel_AgentResult <- dbReadTable(con, Id(schema = schema, table = 'tbl_Creel_AgentResult'))
-  Creel_Agents <- dbReadTable(con, Id(schema = schema, table = 'tbl_Creel_Agents'))
-  Creel_FishCaught <- dbReadTable(con, Id(schema = schema, table = 'tbl_Creel_FishCaught'))
-  Creel_SurveyResult <- dbReadTable(con, Id(schema = schema, table = 'tbl_Creel_SurveyResult'))
-  df.Survey_Agent_Result <- left_join(Creel_SurveyResult, Creel_AgentResult, by = "LoopNumber")
-  df.Fish_Survey_Agent <- left_join(Creel_FishCaught, df.Survey_Agent_Result, by = "SurveyNumber")
+  df.agentresult <- dbReadTable(con, Id(schema = schema, table = 'tbl_Creel_AgentResult'))
+  df.nameagents <- dbReadTable(con, Id(schema = schema, table = 'tbl_Creel_Agents'))
+  df.fishcaught <- dbReadTable(con, Id(schema = schema, table = 'tbl_Creel_FishCaught'))
+  df.survey <- dbReadTable(con, Id(schema = schema, table = 'tbl_Creel_SurveyResult'))
+  df.survey_agent <- left_join(Creel_SurveyResult, Creel_AgentResult, by = "LoopNumber")
+  df.fish_survey_agent <- left_join(Creel_FishCaught, df.Survey_Agent_Result, by = "SurveyNumber")
   
   
   ### Always disconnect and rm connection when done with db
@@ -86,3 +86,104 @@ config <- as.character(R_Config$CONFIG_VALUE)
   
 
 ### SCRIPT BEGIN ####
+
+  
+### Estimated Angler Hours ####
+### rename for testing  
+df.agentresult <- Creel_AgentResult  
+#strptime(df.agentresult$SunUpET, format = "%H:%M:%S", tz = "EST")
+  
+### test as_hms. Gives different Daylight hours value than strptime. Correct seems to be in Estimated Daily Angler Hours.xlsx. possible as_hms converts to seconds, does not account for 60 sec = 1 min, 60 min = 1 hr. ? strptime knows how to handle time.      
+# df.agentresult <- Creel_AgentResult  %>% 
+#   mutate(SunUpET = as_hms(SunUpET), SunDownET = as_hms(SunDownET), LoopStartTimeET = as_hms(LoopStartTimeET), LoopEndTimeET = as_hms(LoopEndTimeET)) %>% 
+#   mutate(DaylightHours = as_hms(SunDownET - SunUpET))
+
+### Daylight Hours   
+df.agentresult <- Creel_AgentResult  %>% 
+  mutate(SunUpET = strptime(SunUpET, format = "%H:%M:%S", tz = "EST"), SunDownET = strptime(SunDownET, format = "%H:%M:%S", tz = "EST"), LoopStartTimeET = strptime(LoopStartTimeET, format = "%H:%M:%S", tz = "EST"), LoopEndTimeET = strptime(LoopEndTimeET, format = "%H:%M:%S", tz = "EST")) %>% 
+  mutate(DaylightHours = as.numeric(difftime(SunDownET, SunUpET))) %>% 
+  mutate(EstAngler_Hrs_loop = DaylightHours*Angler_Count)
+
+### Estimated Daily Angler Hours by Survey Loop - need this for Hours by Month and Year 
+df.dailyhours <- df.agentresult %>% 
+  group_by(LoopDate, Type) %>% 
+  summarise(DailyEst_AnglerHrs=mean(EstAngler_Hrs_loop))
+
+### Estimated Daily Angler Hours by Month and Year - need this for effort calculations  
+df.monthlyhours <- df.dailyhours %>% 
+  group_by(year(LoopDate),month(LoopDate),Type) %>% 
+  summarise(MonthlyEst_AnglerHrs = sum(DailyEst_AnglerHrs))
+
+### Get total number of survey days, AM and PM loops combined. Obsolete after df.survey and df.totals created. Keep for now
+# df.surveydaze <- df.agentresult %>% 
+#   group_by(year(LoopDate), month(LoopDate), Type) %>% 
+#   distinct(LoopDate) %>%
+#   summarise(SurveyDays = n())
+# 
+### Df.survey creates Angler count and Angler survey totals by Date  
+df.survey <- df.agentresult %>% 
+  group_by(LoopDate, year(LoopDate), month(LoopDate), Type) %>%
+  summarise(Angler_Count_sum = sum(Angler_Count), Angler_Surveyed_sum = sum(Anglers_Surveyed)) 
+### df.totals further groups Angler count and Angler survey totals, while also counting number of distinct survey days    
+df.totals <- df.survey %>% 
+  group_by(year(LoopDate), month(LoopDate), Type) %>% 
+  summarise(SurveyDays = n_distinct(LoopDate), Angler_Count_sum = sum(Angler_Count_sum), Angler_Surveyed_sum = sum(Angler_Surveyed_sum))
+
+### Calculate number of Days in the month depending on year 
+### This should work with any new incoming data. Do not want to update year information 
+years.survey <- unique(df.totals$`year(LoopDate)`)
+months.survey <- unique(df.totals$`month(LoopDate)`)
+#months.survey <- paste0("0",unique(df.totals$`month(LoopDate)`))
+
+start <- ymd(paste(years.survey[1], months.survey[1], "1", sep = "-"))
+end <-  ymd(paste(years.survey[2], months.survey[1], "30", sep = "-")) 
+
+Dates1 <- as.Date("2011-01-30") + rep(0, 10)
+Dates2 <- as.Date("2011-02-04") + seq(0, 9)
+Nweekdays <- Vectorize(function(a, b) 
+  sum(!weekdays(seq(a, b, "days")) %in% c("Saturday", "Sunday")))
+Nweekdays(v1, v2)
+
+Dates1 <- as.Date("2011-01-1") + rep(0, 10)
+Dates2 <- Sys.Date() + seq(0, 9)
+Nweekdays <- function(a, b) 
+{
+    dates <- as.numeric((as.Date(a, "%y-%m-%d")):(as.Date(b,"%y-%m-%d")))
+    dates <- dates[- c(1,length(dates))]
+    return(sum(!dates%%7%in%c(0,6)))
+    
+}
+
+Nweekdays(start, end)
+
+sum(!weekdays(seq(a, b, "days")) %in% c("Saturday", "Sunday")))
+
+v1<- seq(from = ymd(19000101), to = ymd(20000101), by='month')
+v2<- seq(from = ymd(20000101), to = ymd(21000101), by='month')
+
+v1 <- seq(from = ymd(paste(years.survey[1], months.survey[1], "1", sep = "-")), to = ymd(as.character(paste(years.survey[1], months.survey[1], "30", sep = "-"))), by='day')
+v2 <- seq(from = ymd(paste(years.survey[2], months.survey[1], "1", sep = "-")), to = ymd(as.character(paste(years.survey[2], months.survey[1], "30", sep = "-"))), by='day')
+
+v1 <- seq(from = ymd(paste(years.survey[1], months.survey[1], "1", sep = "-")), to = ymd(as.character(paste(years.survey[1], months.survey[1], "30", sep = "-"))), by='day')
+v2 <- seq(from = ymd(paste(years.survey[2], months.survey[1], "1", sep = "-")), to = ymd(as.character(paste(years.survey[2], months.survey[1], "30", sep = "-"))), by='day')
+
+
+
+count_weekdays<- Vectorize(function(from,to) sum(!wday(seq(from, to, "days")) %in% c(1,7)))
+ count_weekdays(Dates1, Dates2)
+
+Dates <- seq.Date(from = start, to = end, by = 1) 
+df <- as.data.frame(table(month(Dates), wday(Dates)))
+ 
+
+### this works  
+working_days <- sum(wday(v1)>1 & wday(v1)<7)
+off_days <- sum(wday(v1)==1 | wday(v1)==7)
+
+
+print(working_days)
+
+### testing w filter code 
+# test <- df.survey %>% 
+#   filter(month(LoopDate) == "4" & Type == "Weekend")
+# 
--- Opening Day.R
+++ Opening Day.R
@@ -14,8 +14,11 @@ library(lubridate)
 
 ### create opening day variable 
 year <- paste(as.character(year(Sys.Date())))
+### OR set year 
+# year <- 2017 
 start <- paste(year, "01", "01", sep = "-")
 end <- paste(year, "12", "31", sep = "-")
 x <- seq(ymd(start), ymd(end), by="1 day")
 saturdays <- x[wday(x,label = TRUE) == "Sat" & day(x) <=7]
 openingday <- saturdays[4]
+openingday
